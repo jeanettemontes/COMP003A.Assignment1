63dbaef3316c4fd6456c66aa4558890088d75448
Jeanette Montes <jeanettemontes@my.whccd.edu>
2023-01-18 20:13:26 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,103 @@
+﻿ /*  
+  *  Author Jeanette Montes
+  *  Course: COMP-003
+  *  Purpose: COMP003A.Assignment1
+  */
+ namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 things about me");
+            Console.WriteLine("My name is Jeanette Montes");
+            Console.WriteLine("I am 47 years old");
+            Console.WriteLine("My birthday month is February");
+            Console.WriteLine("Married for 23 years");
+            Console.WriteLine("Mother of four");
+            Console.WriteLine("I have two daughters");
+            Console.WriteLine("And two son");
+            Console.WriteLine("Work for LUESD");
+            Console.WriteLine("as a Technology Support Technician");
+            Console.WriteLine("Graduated WHC in May 2015");
+            Console.WriteLine("In Business Administration");
+            Console.WriteLine("Graduate from Fresno Pacific University in Decemember 2019");
+            Console.WriteLine("Have a Bachelor's degree in Business Administration emphaise Organizational Leadership");
+            Console.WriteLine("Have 9 pugs");
+            Console.WriteLine("Love learning new things");
+            Console.WriteLine("Love spending time with my family");
+            Console.WriteLine("My first car was a 1995 Ford mustang");
+            Console.WriteLine("Grow up in Kettleman City, California");
+            Console.WriteLine("Went to high school in Avenal, California");
+            Console.WriteLine("Started a 21 day no social media challange");
+            Console.WriteLine("I am on day 18 of that challenge");
+            Console.WriteLine("I attend First Pentecostal Church");
+            Console.WriteLine("I am a Sunday School teacher for 7th & 8th graders");
+            Console.WriteLine("Always looking to improve skills that will help me advance in my career");
+            Console.WriteLine("One day I would love to travel to Italy");
+            Console.WriteLine("Favorite food are tacos");
+            Console.WriteLine("Trying to quite drinking Pepsi");
+            Console.WriteLine("Back to my pugs");
+            Console.WriteLine("I first got Pugsley in 2017");
+            Console.WriteLine("Noticed he was lonely so I got Bella in 2018");
+            Console.WriteLine("Pugsley and Bella had their first litter in 2020");
+            Console.WriteLine("Here are the names of their litter by birth order");
+            Console.WriteLine("Wednesday, I like to refer her to \"first born\"");
+            Console.WriteLine("Then there's Blue, Ruby, Fonzy, Gigi, Benny, and Penelope");
+            Console.WriteLine("I love animals espeically horses");
+            Console.WriteLine("Horse are to expensive which is why I don't have one");
+            Console.WriteLine("Favorite color is blue");
+            Console.WriteLine("Struggle in Biology");
+            Console.WriteLine("I enjoy going bowling with the family");
+            Console.WriteLine("I'm looking forward to learning a lot in this class");
+            Console.WriteLine("I once won a dance competition in my early 20s");
+            Console.WriteLine("Got second place in a high school dance compentition");
+            Console.WriteLine("Married my 3rd grade crush");
+            Console.WriteLine("I used to want to be a high school counselor");
+            Console.WriteLine("In high school I thought I want to go into cosmetology");
+            Console.WriteLine("Took my first computer course in high school my sophmore year");
+            Console.WriteLine("I wanted to name my daughter Wednesday but my husband would like me");
+            Console.WriteLine("Purchased our second RV last year but now looking to selling it");
+            Console.WriteLine("We don't camp as much as we use to");
+            Console.WriteLine("I like coffee");
+            Console.WriteLine("Favorite drink at Starbucks is White Chocolate Mocha");
+            Console.WriteLine("Second favorite drink at Starbucks is an iced chai latte");
+            Console.WriteLine("I have brown eyes");
+            Console.WriteLine("Brown hair with lots of gray");
+            Console.WriteLine("I wear glasses only to see far");
+            Console.WriteLine("I will no longer eat a McDonald's");
+            Console.WriteLine("Had my first kid at 17 years old");
+            Console.WriteLine("Like to go in hikes");
+            Console.WriteLine("Like watching the Office when cleaning the house");
+            Console.WriteLine("Like to camp especially with friends");
+            Console.WriteLine("Enjoy to to Universal Studios during Christmas to see the Grinch");
+            Console.WriteLine("We found a short-cut into the Harry Potter ride");
+            Console.WriteLine("Now that my kids are older we all get in the single rider line at Uinversal Studio");
+            Console.WriteLine("The single rider lines for the rides are a lot shorter");
+            Console.WriteLine("Last time I was at Disneyland was 1996");
+            Console.WriteLine("This year I will be married for 24 years");
+            Console.WriteLine("This is kind of fun");
+            Console.WriteLine("Being working at my current job for 6 years and 7 months");
+            Console.WriteLine("I have a really nice boss");
+            Console.WriteLine("Just learned my boss is ordering me a cell phone for work");
+            Console.WriteLine("I started working for the school system in January 2000");
+            Console.WriteLine("I started as an Office Aide");
+            Console.WriteLine("In 2003 got hired as a Clerk I at the Hanford Adult School");
+            Console.WriteLine("In June 2007 was hired at Lemoore Union Elementary School District Univeristy Charter School as their School Clerk");
+            Console.WriteLine("Got a secretary position at Pioneer Middle School in 2015");
+            Console.WriteLine("It was a part time position");
+            Console.WriteLine("Went back to work with Lemoore Union Elementary School District in 2016");
+            Console.WriteLine("Where I am currently work at");
+            Console.WriteLine("My children are now adults");
+            Console.WriteLine("I still worry about them");
+            Console.WriteLine("I have no grandchildren");
+            Console.WriteLine("I have a gym membership but hardly use it");
+            Console.WriteLine("I love to swimming");
+            Console.WriteLine("Bought our first home in October 1999");
+            Console.WriteLine("Sold it in 2016");
+            Console.WriteLine("Purchased our second home in 2017");
+            Console.WriteLine("My current home has a big back yard and I finally go a pool");
+            Console.WriteLine("And a spa");
+        }
+    }
+}
